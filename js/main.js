93a82f6899a28c9a8e829435494e0c85dfa00657
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-08-10 10:39:59 -0700

    Site is done.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+(function ($) {
+  "use strict";
 
+  var
+  none = { background: "white", foreground: "black" },
+  services = {
+    "Twitter"   : { background: "#0096ff", foreground: "white" },
+    "GitHub"    : { background: "#242424", foreground: "white" },
+    "LinkedIn"  : { background: "#367da6", foreground: "white" },
+    "Google+"   : { background: "#00bc66", foreground: "white" },
+    "YouTube"   : { background: "#942424", foreground: "white" },
+    "SoundCloud": { background: "#ff6600", foreground: "white" },
+    "Email"     : { background: "#778b93", foreground: "white" }
+  },
+  introNav = $(".intro nav"),
+  cursor = $(".intro div"),
+  setTheme = function (theme) {
+    $("html").css({
+      "background-color": theme.background,
+      "color": theme.foreground
+    });
+    $(".intro li a, .intro div").attr("class", "").addClass(theme.foreground);
+  },
+  moveCursor = function (position, width) {
+    cursor.css({
+      "opacity": "0.5",
+      "left": position.left + "px",
+      "width": width + "px"
+    });
+  };
+
+  $(function () {
+    introNav.mouseleave(function () {
+      cursor.css("opacity", "0");
+      setTheme(none);
+    });
+
+    $(".intro li").mouseenter(function () {
+      var
+      $this       = $(this),
+      serviceName = $this.children().html(),
+      serviceStyle = services[serviceName];
+      
+      setTheme(serviceStyle);
+      moveCursor($this.position(), $this.width());
+    });
+
+    $("[data-tooltip]").tooltipster({
+      delay: 0,
+      theme: ".simple-black"
+    });
+  });
+})(window.jQuery);
