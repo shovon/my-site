3b47c280ffe4a1485119e53d725609569e2b9093
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-10-01 08:52:13 -0700

    Did a boat load of stuff for the footer.

--- contents/js/layout.js
+++ contents/js/layout.js
@@ -2,11 +2,29 @@
   'use strict';
 
   $(function () {
-    var $siteHeader = $('.site-header li');
-    $siteHeader.tooltipster({
+    var $footer = $('body > footer');
+    var $footerSocial = $footer.find('.social-footer li');
+    $footerSocial.tooltipster({
       delay: 0,
       theme: '.tooltip-simple-black-small'
     });
+
+    setFooter();
+
+    $(window).resize(function () {
+      setFooter();
+    });
+
+    function setFooter() {
+      var footerHeight =
+        ($footer.hasClass('push-down') && $footer.height()) || 0;
+
+      if (($('body').height() + footerHeight) < $(window).height()) {
+        $footer.addClass('push-down');
+      } else {
+        $footer.removeClass('push-down');
+      }
+    }
   });
 
 })(window.jQuery);
