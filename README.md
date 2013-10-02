a056c8d5c99f672263f2c867620c0bf352c8e849
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-10-01 22:42:49 -0700

    Some more styling.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-
 # Blog
 
-The default [wintersmith](https://github.com/jnordberg/wintersmith) template
+A lot of the code is based on the default [wintersmith](https://github.com/jnordberg/wintersmith) template
+
+# TODO:
+
+- soft code the hostname onto the configs. This allows Disqus to properly link to specific articles in the blog. There's also potentially other benefits as well
--- contents/js/home.js
+++ contents/js/home.js
@@ -2,9 +2,13 @@
   'use strict';
 
   $(function () {
-    var $hero = $('.hero-unit');
-    var $nav = $hero.find('hgroup ul');
+    var $hero = $('body > section');
+    var $headerNav = $('body > header');
+    var $intro = $hero.find('hgroup');
+    var $nav = $intro.find('ul');
     var $navEl = $nav.find('[data-tooltip]');
+    var $window = $(window);
+
     $nav.mouseleave(function () {
       $hero.css({'background-color': ''}).removeClass('white');
     });
@@ -16,6 +20,31 @@
       delay: 0,
       theme: '.tooltip-simple-black'
     });
+
+    $window.resize(function () {
+      resizeHero();
+    });
+
+    resizeHero();
+
+    function resizeHero() {
+      var headerHeight = $headerNav.height();
+      var windowHeight = $window.height();
+
+      var introHeight = $intro.height();
+
+      var newHeroHeight = windowHeight - headerHeight;
+
+      if (newHeroHeight < introHeight + 50) {
+        newHeroHeight = introHeight + 50;
+      }
+
+      var newIntroPadding = (newHeroHeight / 2) - (introHeight / 2);
+
+      $hero.height(newHeroHeight);
+
+      $intro.css('padding-top', (newIntroPadding) + 'px');
+    }
   });
 
 }(window.jQuery));
