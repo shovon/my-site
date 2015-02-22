85f87abb6475b17699c6ffafe545bf89dc62855c
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2015-02-22 10:40:47 -0800

    Initial commit.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-$('.social a').mouseover(function () {
-  $(this)
-}).mouseleave(function () {
-  
-});
+;(function () {
+  var $html = $('html');
+
+  $('.social').mouseleave(function () {
+    $html.css('background-color', 'transparent');
+    $html.removeClass('hovering');
+  });
+
+  $('.social a').mouseover(function () {
+    var color = $(this)
+      .find('span')
+      .attr('style')
+      .split(',')
+      .filter(function (attribute) {
+        return attribute.split(':')[0].trim() === 'background-color';
+      })[0] || 'background-color: black';
+    color = color.split(':')[1] || 'black'; color = color.trim();
+    if (color[color.length - 1] === ';') {
+      color = color.slice(0, color.length - 1);
+      color = color.trim();
+    }
+    $html.css('background-color', color);
+    $html.addClass('hovering');
+  });
+})();
